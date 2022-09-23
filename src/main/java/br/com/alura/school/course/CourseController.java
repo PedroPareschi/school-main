89a7383b59ab11a8c0acb34760c62a4c100d6c9d
pedropareschi <pedropareschi15@gmail.com>
2022-09-23 19:42:41 -0300

    Correção dos testes automatizados: removendo import

--- src/main/java/br/com/alura/school/course/CourseController.java
+++ src/main/java/br/com/alura/school/course/CourseController.java
@@ -7,7 +7,6 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
